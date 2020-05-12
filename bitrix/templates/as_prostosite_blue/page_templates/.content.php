e431c5209edf8e77763aa421a87382f25b13ac56
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-05-12 22:16:40 +0300

    add main include

--- bitrix/templates/as_prostosite_blue/page_templates/.content.php
+++
@@ -1,6 +0,0 @@
-<?
-if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
-
-IncludeTemplateLangFile(__FILE__);
-
-$TEMPLATE["sect_main.php"] = Array("name" => GetMessage("sect_main"), "sort" => 3);
