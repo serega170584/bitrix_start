3b4f93c040437a69b5a4e1bf8ffb84861d652bfe
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-05-14 12:42:57 +0300

    add contacts company

--- contacts/index.php
+++ contacts/index.php
@@ -7,12 +7,11 @@ $APPLICATION->SetTitle("Контакты");
   <tr>
     <td>
     <div class="vcard">
-    	<div><span class="category">Организация</span> <span class="fn org">Ромашка</span></div>
-        <div><b>Телефон:</b> <span class="tel">+7 (000) 000-00-00</span></div>
-        <div class="adr"><b>Адрес:</b> 000000, <span class="locality">г. Солнечный</span>, <span class="street-address">просп. Романтиков, д. 21</span></div>
-        <div><b>E-mail:</b> <a href="mailto:noreply@mysite.com">noreply@mysite.com</a></div>
-        <div>Мы работаем <span class="workhours">ежедневно с 11:00 до 24:00</span></div>
-        <span class="url"><span class="value-title" title="http://www.mysite.com"></span></span>
+    	<div><span class="category">Организация</span> <span class="fn org">Serjstudio</span></div>
+        <div><b>Телефон:</b> <span class="tel">+7 (962) 981-51-63</span></div>
+        <div class="adr"><b>Адрес:</b> 141707, <span class="locality">г. Долгопрудный Московской области</span>, <span class="street-address">ул. Корабельная, д. 1</span></div>
+        <div><b>E-mail:</b> <a href="mailto:serega170584@gmail.com">serega170584@gmail.com</a></div>
+        <div>Мы работаем <span class="workhours">ежедневно с 9:00 до 18:00</span></div>
     </div>
     </td>
     <td align="right">
@@ -22,7 +21,7 @@ $APPLICATION->SetTitle("Контакты");
         "",
         Array(
             "INIT_MAP_TYPE" => "MAP",
-            "MAP_DATA" => "a:3:{s:10:\"yandex_lat\";s:7:\"55.7383\";s:10:\"yandex_lon\";s:7:\"37.5946\";s:12:\"yandex_scale\";i:10;}",
+            "MAP_DATA" => "a:3:{s:10:\"yandex_lat\";s:7:\"55.9603\";s:10:\"yandex_lon\";s:7:\"37.5053\";s:12:\"yandex_scale\";i:10;}",
             "MAP_WIDTH" => "600",
             "MAP_HEIGHT" => "300",
             "CONTROLS" => array("ZOOM", "MINIMAP", "TYPECONTROL", "SCALELINE"),
