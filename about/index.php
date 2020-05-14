70729bf641d6f9e0db78a53793e04f899113edb8
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-05-14 11:52:36 +0300

    add about company

--- about/index.php
+++ about/index.php
@@ -18,4 +18,12 @@ $APPLICATION->SetTitle("О компании");
         </li>
     </ul>
     </p>
+    <h2>Примеры работ</h2>
+<?php $APPLICATION->IncludeComponent("ambersite:gallery", "", array(
+        "CACHE_TYPE" => "A",
+        "CACHE_TIME" => "360000",
+        "IBLOCK_CODE" => "as_gallery_s1",
+        "PROP" => "AS_GAL_PICT"
+    )
+); ?>
 <? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
